a634af338521df98a51c62eed9794c24f22d3466
jonathan <lexusjt98@gmail.com>
2023-02-08 01:02:57 -0500

    Node server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Hola, mundo")
+const express = require('express')
+const app = express()
+const port = 3001
+
+app.get ("/", (req, res) =>{
+  res.send("HOla, mundo")
+})
+
+app.listen(port, ()=>{
+  console.log("Mi puerto es "+ port)
+})
+
